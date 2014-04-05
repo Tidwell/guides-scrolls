897d83475c2ef8d6e1d40c523d6cd7580c7754f0
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2014-04-04 23:38:22 -0400

    header

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,29 +1,34 @@
-$(function() {
+(function(window, $, undefined) {
 	'use strict';
 
-	// Load the IFrame Player API code asynchronously.
-	var tag = document.createElement('script');
-	tag.src = 'https://www.youtube.com/player_api';
-	var firstScriptTag = document.getElementsByTagName('script')[0];
-	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-	$('.video-container').click(function() {
+	function loadVideo() {
 		var $container = $(this);
-		$container.find('.play-container, .content-container, .type-tag').hide();
 		$container.addClass('playing');
 
-		var $frame = $container.find('.player-placeholder');
-		$frame.attr('id', 'video'+Math.floor(Math.random()*10000));
+		var $placeholder = $container.find('.player-placeholder');
+		$placeholder.attr('id', 'video'+Math.floor(Math.random()*10000));
 
-		new YT.Player($frame.attr('id'), {
-			videoId: $frame.attr('data-video-id'),
+		new window.YT.Player($placeholder.attr('id'), {
+			videoId: $placeholder.attr('data-video-id'),
 			playerVars: {
 				autoplay: 1
 			}
 		});
 
-		$frame = $container.find('iframe').show();
-
 		return false; //stop prop and prev def
-	});
-});
+	}
+
+	function init() {
+		// Load the Youtube IFrame Player API code asynchronously.
+		var tag = document.createElement('script');
+		tag.src = 'https://www.youtube.com/player_api';
+		var firstScriptTag = document.getElementsByTagName('script')[0];
+		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+		//bind 
+		$('.content-block').on('click', '.video-container', loadVideo);
+	}
+
+
+	$(init);
+}(window, jQuery));
